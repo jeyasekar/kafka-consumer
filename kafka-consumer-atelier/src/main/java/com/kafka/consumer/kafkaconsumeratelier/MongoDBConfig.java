d157a234828217d8c824427d05c9616724a59e02
jsanthan <jsanthan@GOT500VI75300.vccnet.volvocars.net>
2019-06-03 17:44:46 +0530

    upsert mongodb

--- kafka-consumer-atelier/src/main/java/com/kafka/consumer/kafkaconsumeratelier/MongoDBConfig.java
+++ kafka-consumer-atelier/src/main/java/com/kafka/consumer/kafkaconsumeratelier/MongoDBConfig.java
@@ -1,17 +1,31 @@
 package com.kafka.consumer.kafkaconsumeratelier;
 
-import com.kafka.consumer.mongodb.repository.WeatherRepository;
-import com.kafka.consumer.service.WeatherMetrics;
-import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+import com.kafka.consumer.mongodb.repository.WeatherRepository;
+import com.mongodb.MongoClient;
 
 @EnableMongoRepositories(basePackageClasses = WeatherRepository.class)
 @Configuration
 public class MongoDBConfig {
+	@Value("${spring.data.mongodb.host}")
+	private String host;
+	@Value("${spring.data.mongodb.database}")
+	private String db;
 
+	@Bean
+	public MongoClient mongo() {
+		return new MongoClient(host);
+	}
 
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongo(), db);
+	}
 	/*
 	 * @Bean CommandLineRunner commandLineRunner(WeatherRepository userRepository) {
 	 * return strings -> { userRepository.save(new Weather());
--- kafka-consumer-atelier/src/main/java/com/kafka/consumer/service/ConsumerService.java
+++ kafka-consumer-atelier/src/main/java/com/kafka/consumer/service/ConsumerService.java
@@ -1,10 +1,15 @@
 package com.kafka.consumer.service;
 
+import java.util.HashMap;
 import java.util.concurrent.CountDownLatch;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
@@ -22,6 +27,9 @@ public class ConsumerService {
 	
 	@Autowired
 	private WeatherRepository userRepository;
+	
+	@Autowired
+	private MongoTemplate template;
 	public CountDownLatch getLatch(){
 		
 		return latch;
@@ -38,18 +46,34 @@ public class ConsumerService {
 	 */
 	public void insert() {
 		 WeatherMetrics dat=new WeatherMetrics();
-		 dat.setCity("sgsdg");
-		 userRepository.save(dat);
+		 dat.setCity("chennai");
+		 HashMap<String, CityWeather> map = new HashMap<String, CityWeather>();
+		 CityWeather city=new CityWeather();
+		 city.setCurrTemp(80.78);
+		 map.put("one", city);
+		 upsert(dat);
 	}
 	 @KafkaListener(topics = "meenatopic")
 	    public void receive(@Payload WeatherMetrics data,
 	                        @Headers MessageHeaders headers) {
 		 LOGGER.info("received DataModel='{}'", data);
-		 userRepository.save(data);
+		// userRepository.save(data);
+		 template.save(data, "AllTemp");
+		 upsert(data);
 	        headers.keySet().forEach(key -> {
 	        	LOGGER.info("{}: {}", key, headers.get(key));
 	        });
 	    }
+
+	private void upsert(WeatherMetrics data) {
+		Query query = new Query();
+		query.addCriteria(Criteria.where("city").is(data.getCity()));
+
+		WeatherMetrics weather = template.findOne(query, WeatherMetrics.class);
+		Update update = new Update();
+        update.set("map.currTemp", data.getTemp());
+		template.upsert(query, update, weather.getClass(), "LatestTemp");
+	}
 
 }
 	
